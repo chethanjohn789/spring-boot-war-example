5e7ecab7998334eb7d8f18f596448368ded5b73f
chethanjohn789 <113040922+chethanjohn789@users.noreply.github.com>
2022-10-16 12:45:57 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "This is My First Deployment In Tomcat9 by using Jenkins/n Thank you Sudhakar sir";
+        return "This is My First Deployment In Tomcat9 by using Jenkins/n Thank you Sudhakar and Rathnakar sir";
     }
 
 }
